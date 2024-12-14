f1b9629719d20e61de2722dff2d4c97db979897e
Santhosh Kumar <ksdsanthosh130@gmail.com>
2024-12-14 13:14:58 +0530

    Day 1

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+function Message () {
+    const name = "Santhosh";
+    
+    if (name) 
+        return<h1 className="text-3xl font-bold underline">Hello {name}!</h1>
+        return <h1> Hello Wolrd!</h1>
+      
+
+}
+export default Message;
